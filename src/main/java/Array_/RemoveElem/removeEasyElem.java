6776e25208d119053bf09e8eb1d080d535765bdb
Ruan <2789051355@qq.com>
2023-01-11 23:20:07 +0800

    Fist commit

---
+++ src/main/java/Array_/RemoveElem/removeEasyElem.java
@@ -0,0 +1,19 @@
+package Array_.RemoveElem;
+
+public class removeEasyElem {
+}
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int lowIndex = 0;
+        int fasetIndex = 0;
+        int size = nums.length;
+        for(fasetIndex = 0; fasetIndex < nums.length; fasetIndex ++ ) {
+            //快指针往后移动， 快指针遇到val时向后移动
+            //慢指针在快指针遇到val时不动，没遇到val时赋值
+            if(nums[fasetIndex] != val) {
+                nums[lowIndex++] = nums[fasetIndex];
+            }
+        }
+        return lowIndex;
+    }
+}
