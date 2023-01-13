eeed78d75811e6a78a5f2021ac8d631995c4560f
Ruan <2789051355@qq.com>
2023-01-13 20:36:03 +0800

    Total Fruit v2

--- src/main/java/Array_/SlidingWindow/TotalFruit.java
+++ src/main/java/Array_/SlidingWindow/TotalFruit.java
@@ -4,7 +4,6 @@ package Array_.SlidingWindow;
 public class TotalFruit {
     public static int totalFruit(int[] fruits) {
         int result = Integer.MIN_VALUE;
-        int sublength = 0;
         //使用数组去存储，下标代表种类，下标对应的值是种类的个数
         int[] cnt = new int[fruits.length +  1];
         for(int j = 0, i = 0, tol = 0; j < fruits.length; j++) {
