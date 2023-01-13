01e1760d3f54d09ae9c35ad450b2ca2e22b19678
Ruan <2789051355@qq.com>
2023-01-14 00:18:01 +0800

    TotalFruit v2

--- src/main/java/Array_/SlidingWindow/TotalFruit.java
+++ src/main/java/Array_/SlidingWindow/TotalFruit.java
@@ -2,6 +2,11 @@ package Array_.SlidingWindow;
 
 // leetcode 904:https://leetcode.cn/problems/fruit-into-baskets/
 public class TotalFruit {
+
+    public static void main(String[] args) {
+        System.out.println(totalFruit1(new int[]{3,3,3,1,2,1,1,2,3,3,4}));
+    }
+
     public static int totalFruit(int[] fruits) {
         int result = Integer.MIN_VALUE;
         //使用数组去存储，下标代表种类，下标对应的值是种类的个数
@@ -9,7 +14,7 @@ public class TotalFruit {
         for(int j = 0, i = 0, tol = 0; j < fruits.length; j++) {
             //等于1代表种类新增一种
             if (++cnt[fruits[j]] == 1) tol++;
-            //种类为三种时开始重置
+            //种类为三种时开始滑动
             while (tol > 2) {
                 if (--cnt[fruits[i++]] == 0) tol--;
             }
@@ -18,5 +23,23 @@ public class TotalFruit {
         return result;
     }
 
-
+    public static int totalFruit1(int[] fruits) {
+        int result = Integer.MIN_VALUE;
+        //使用数组去存储，下标代表种类，下标对应的值是种类的个数
+        int total = 0;
+        for(int i = 0; i < fruits.length; i++) {
+            int[] cnt = new int[fruits.length +  1];
+            for (int j = i; j < fruits.length; j++) {
+                if (++cnt[fruits[j]] == 1) {
+                    total++;
+                }
+                if (total > 2) {
+                    result = result > j - i  ? result : j - i ;
+                    total = 0;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
